92881bf1b8b68da9d5d193270803e63fb7abc9f5
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2019-05-18 00:39:28 +0200

    chore(HomeScreen): add first container

---
+++ src/containers/HomeScreen/Wrapper.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background: limegreen;
+  margin: 1em;
+`;
+
+export default Wrapper;
---
+++ src/containers/HomeScreen/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Wrapper from './Wrapper';
+
+class HomeScreen extends Component{
+  render(){
+    return(
+      <Wrapper>
+        HomeScreen
+      </Wrapper>
+    );
+  }
+}
+
+export default HomeScreen;
