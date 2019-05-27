6b208d65d2131d1ab07b6213eef500a28e482456
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2019-05-27 22:12:08 +0200

    chore(styles): add first initial color scheme

---
+++ src/styles/variables.js
@@ -0,0 +1,8 @@
+export const YELLOW = '#ebbe00'
+export const APPLE_GREEN = '#89b635'
+export const GREEN = '#29a161'
+export const TURQUOISE = '#008679'
+export const BLUE = '#006776'
+export const DARK_BLUE = '#2f4858'
+export const WHITE = '#f6f6f6'
+export const BLACK = '#333333'
