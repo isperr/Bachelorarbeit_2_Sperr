f7409723be935fe123acbc0d28b118c845dc5842
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2019-06-12 16:58:31 +0200

    feat(QuestionComponents): add first styling for answer options

---
+++ src/components/QuestionComponents/AllAnswers.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import {TURQUOISE} from '../../styles/variables';
+
+const AllAnswers = styled.div`
+  background: ${TURQUOISE};
+  min-height: 10rem;
+  width: 90%;
+`;
+
+export default AllAnswers;
---
+++ src/components/QuestionComponents/AnswerWrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import {YELLOW} from '../../styles/variables';
+
+const AnswerWrapper = styled.div`
+  background: ${YELLOW};
+  padding: 1rem 1rem;
+  margin: 0.5rem;
+  border-radius: 0.5rem;
+`;
+
+export default AnswerWrapper;
