efc51c9fbeda006e71edbc726164d0849d3f630f
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2019-06-13 22:22:19 +0200

    fix(HomeScreen): fix style

--- src/containers/HomeScreen/Wrapper.js
+++ src/containers/HomeScreen/Wrapper.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
+import {WHITE} from '../../styles/variables';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  background: ${WHITE};
+  min-height: stretch;
 `;
 
 export default Wrapper;
