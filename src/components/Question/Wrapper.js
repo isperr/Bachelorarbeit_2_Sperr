a6a87df2220385226ee8684ac915adb96ef19616
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2019-06-13 22:24:11 +0200

    fix(Question): fix wrapper style

--- src/components/Question/Wrapper.js
+++ src/components/Question/Wrapper.js
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
-import {BLUE} from '../../styles/variables';
+import {GREEN} from '../../styles/variables';
 
 const Wrapper = styled.div`
-  background: ${BLUE};
+  background: ${GREEN};
   width: 70%;
   padding: 1em;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 2rem;
+  border-radius: 1rem;
+  min-height: 22rem;
+  justify-content: center;
 `;
 
 export default Wrapper;
