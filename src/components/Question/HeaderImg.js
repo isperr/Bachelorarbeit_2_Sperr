1958348862020d737bcecbb9ccdf0f6b026be958
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2019-06-16 22:47:34 +0200

    feat(Question): add first styling for images in questions and answers

---
+++ src/components/Question/HeaderImg.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HeaderImg = styled.img`
+  height: 15rem;
+  border-radius: 1rem;
+  filter: drop-shadow(0px 3px 9px #6f6f6f);
+  margin-top: 1.5rem;
+  margin-bottom: 1rem;
+`;
+
+export default HeaderImg;
