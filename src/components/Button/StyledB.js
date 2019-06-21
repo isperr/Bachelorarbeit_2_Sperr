d3da500ee8bbf4940d38d4901f99e788184983a3
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2019-06-21 16:12:18 +0200

    fix(Button): adapt button style

--- src/components/Button/StyledB.js
+++ src/components/Button/StyledB.js
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
-import {YELLOW} from '../../styles/variables';
+import {APPLE_GREEN} from '../../styles/variables';
 
 const StyledB = styled.button`
-  background: ${YELLOW};
-  padding: 1em;
+  background: ${APPLE_GREEN};
+  padding: 0.75em 1em;
+  margin-bottom: 2em;
   border: none;
   border-radius: 1.5em;
-  box-shadow: 0 9px #999;
+  box-shadow: 0 9px #999999b5;
   /* box-shadow: 1px 8px 4px #999; */
   outline: none;
+  font-size: smaller;
+  min-width: 7rem;
 
   :hover{
   }
