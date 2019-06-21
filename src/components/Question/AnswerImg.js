d532578dfc6524ca7cd407342ba3f87cb48589ba
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2019-06-21 16:18:18 +0200

    fix(Question): remove margin for answer img

--- src/components/Question/AnswerImg.js
+++ src/components/Question/AnswerImg.js
@@ -4,8 +4,6 @@ const AnswerImg = styled.img`
   height: 4rem;
   border-radius: 1rem;
   filter: drop-shadow(0px 3px 9px #6f6f6f);
-  margin-top: 1.5rem;
-  margin-bottom: 1rem;
 `;
 
 export default AnswerImg;
